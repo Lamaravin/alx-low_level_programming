e19103985cb135982808496888edaa50f880c5af
Ahaneku_Laura <laurahaneku>
2022-04-12 07:08:28 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
  * main - main block
  * Description:print -putchar in a new line
- * Return: Always 0 (Success)
+ * Return: this program returns zero
  */
 int main(void)
 {
