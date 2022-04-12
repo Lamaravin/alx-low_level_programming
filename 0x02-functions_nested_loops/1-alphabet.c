e57a051ab23dba137bd5b8dee9a6970b2e5fa162
Ahaneku_Laura <laurahaneku>
2022-04-12 05:33:17 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet in lowercase
+ *
+ **/
+void Print_alphabet(void);
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+}
