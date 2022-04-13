0ed76daa95dbb458807d7c69ea237ee95f5faa31
Ahaneku_Laura <laurahaneku>
2022-04-13 01:12:24 -0700

    Task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,28 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * _abs - return abs value
+ * _abs - return absolute value
+ *
  * @n : number to check
- * Return:0 or 1
+ * Return: Always 0 
  */
+
 int _abs(int n)
 {
+if (n < 0)
+{
+return (n * (-1));
+}
 
-return (n >= 0 ? n : n * -1);
+else if (n == 0)
+{
+return (0);
+}
+
+else
+{
+return (n);
+}
 
 }
