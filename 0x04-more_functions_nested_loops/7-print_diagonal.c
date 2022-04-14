289571c5d2d9afe069e6e8f890bbe76bbd56e4d1
Ahaneku_Laura <laurahaneku>
2022-04-14 12:07:04 -0700

    God blessing

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,18 +7,19 @@
  */
 void print_diagonal(int n)
 {
-	int i = 0, j;
+int i = 0, j;
 
-	if (n > 0)
-	{
-		for (; i < n; i++)
-		{
-			for (j = 0; J < i; j++)
-				_putchar(' ');
-			_putchar(92);
-			_putchar('\n');
-		}
-	}
-	else
-		_putchar('\n');
+if (n > 0)
+{	
+for (; i < n; i++)
+{
+for (j = 0; J < i; j++)
+
+_putchar(' ');
+_putchar(92);
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
 }
