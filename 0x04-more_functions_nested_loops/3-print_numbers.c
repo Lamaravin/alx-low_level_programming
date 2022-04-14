317f27ce204a2c21ba52eff8d35f49cc3404935c
Ahaneku_Laura <laurahaneku>
2022-04-14 08:17:40 -0700

    Laura commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
+
 /**
- * print_numbers - print numbers
- * Return: Always 0 (Success)
+ * print_numbers - print numbers 0-9
+ *
+ * Return: void
  */
 
 void print_numbers(void)
 {
-int i;
+int c;
 
-i = 0;
-while (i < 10)
+for (c = '0'; c <= '9' c++)
 {
-_putchar(i + '0');
-i++;
+_putchar(c);
 }
 
 _putchar('\n');
