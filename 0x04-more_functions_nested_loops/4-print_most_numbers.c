3d9f40b269c1e44bd87e02ba51fdd000faa9e6e8
Ahaneku_Laura <laurahaneku>
2022-04-14 05:04:56 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers
+ * Return: Always 0 (Success)
+ */
+
+void print_most_numbers(void)
+{
+
+int 1;
+
+i = 0;
+while (i < 10)
+{
+if (i != 2 && i != 4)
+{
+_putchar (i + '0')
+}
+i++;
+}
+
+_putchar ('\n');
+}
+
