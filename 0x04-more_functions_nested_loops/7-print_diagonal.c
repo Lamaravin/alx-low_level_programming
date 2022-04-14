8d270a64973d6a05f77b98351e90799b5241dbf9
Ahaneku_Laura <laurahaneku>
2022-04-14 14:02:44 -0700

    it will work

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,25 +1,30 @@
 #include "main.h"
 
 /**
- * print_diagonal - a function that draws a diagonal line on the terminal
- * @n: An input integer
- * Return: Always 0
+ * print_diagonal - prints diagonals
+ * @n: limit
+ *
+ * Return: void
  */
+
 void print_diagonal(int n)
 {
-int i = 0, j;
+int i, j;
 
 if (n > 0)
-{	
-for (; i < n; i++)
 {
-for (j = 0; J < i; j++)
-
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
 _putchar(' ');
-_putchar(92);
+}
+_putchar('\\');
 _putchar('\n');
 }
 }
 else
+{
 _putchar('\n');
+}
 }
