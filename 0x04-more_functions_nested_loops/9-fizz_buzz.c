cb4a0e333880c7f07c1da9d80aad84d0116d90c4
Ahaneku_Laura <laurahaneku>
2022-04-14 21:24:01 -0700

    Love

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,7 @@ int i;
 i = 1;
 while (i <= p)
 {
-if (i % 3 == 0 && 1 % 5 == 0)
+if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz ");
 }
@@ -35,7 +35,7 @@ printf("Buzz ");
 
 else
 {
-prinf("%1 ", i);
+prinf("%i ", i);
 }
 
 i++;
