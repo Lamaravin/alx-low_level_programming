54416ab77781b02724c296c2808a8e46c15de3f6
Ahaneku_Laura <laurahaneku>
2022-04-26 07:43:33 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _memcpy -   copy number bytes form adress "from" to adress "to"
- * *@dest: content is to be copied
- * *@src: Source of data to be copied
+ * @dest: content is to be copied
+ * @src: Source of data to be copied
  * @n: bytes of the memory
  * Return: dest
  */
@@ -11,7 +11,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 {
 	unsigned int i;
-	
+
 	i = 0;
 	while (i < n)  /*Delacring WHILE*/
 	{
