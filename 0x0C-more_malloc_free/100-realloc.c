f547a379a1140cbc11a309697b58d18bf3e59dee
Ahaneku_Laura <laurahaneku>
2022-05-09 07:41:57 -0700

    advanced task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,16 +15,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr != NULL)
 		clone = ptr;
 	else
-	{
-		return (malloc(new_size));
-	}
+	{ return (malloc(new_size)); }
 	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
-	{ 
-		free(ptr);
-		return (0);
-	}
+	{ free(ptr);
+		return (0); }
 	relloc = malloc(new_size);
 	if (relloc == NULL)
 		return (0);
